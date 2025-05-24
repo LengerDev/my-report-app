e5e66eabddf007d00a3cb15377c62338a0d437d1
Lim Lenger <lengerdev@Lims-MacBook-Pro.local>
2025-05-24 09:33:50 +0700

    Add Dashboard Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+// import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  // const isLoggedIn = true;
+
+  // if (!isLoggedIn) {
+  //   return redirect("/login");
+  // } else {
+  //   redirect("/dashboard/overview");
+  // }
+
+  return <h2>Dashboard Page</h2>;
+}
