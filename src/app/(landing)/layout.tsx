86b03b6c521ecd3cf628d965eae2818ae4e33d0d
Lim Lenger <lengerdev@Lims-MacBook-Pro.local>
2025-05-26 17:29:02 +0700

    Fixed scroll issue on landing page

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,9 +2,9 @@ import Navbar from "@/components/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <header className="bg-white">
+    <div className="bg-white">
       <Navbar />
       {children}
-    </header>
+    </div>
   );
 }
